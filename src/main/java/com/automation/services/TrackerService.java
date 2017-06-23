9a2a9c60adc3222f2adab042e0f106cbfaf05b84
Selvamani T <selvamani.t@cognizant.com>
2017-06-23 15:59:21 +0530

    Updated REST Services

--- src/main/java/com/automation/services/TrackerService.java
+++ src/main/java/com/automation/services/TrackerService.java
@@ -1,5 +1,6 @@
 package com.automation.services;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -52,23 +53,24 @@ public class TrackerService {
 		return Response.status(200).entity(agent).build();
 	}
 
-	/**
-	 * Stores Agent idle hours from extension. agentName, EmailId,
-	 * (IdleHours)From, To, WebsitesUsed sampleUrl ::
-	 * http://localhost:8082/TimeTracker/rest/agent/
-	 * 
-	 * @return
-	 * @throws ValidationException
-	 * @throws CustomException
-	 */
 	@POST
-	@Path("/{serviceName}/{agentName}/{agentEmailId}/{productivityHours}/{loginTime}/{logoutTime}/{idleTimings}")
-	public Response storeAgentData(@PathParam("serviceName") String serviceName,
-			@PathParam("agentName") String agentName, @PathParam("agentEmailId") String agentEmailId,
-			@PathParam("productivityHours") String productivityHours, @PathParam("loginTime") String loginTime,
-			@PathParam("logoutTime") String logoutTime, @PathParam("idleTimings") String idleTimings)
-			throws ValidationException, CustomException {
-		System.out.println(AppConstants.METHOD + "-storeAgentData()");
+	@Path("/info")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response storeData(Agent jsonagent) {
+		System.out.println(AppConstants.METHOD + "-getAgentData()*************");
+		System.out.println(jsonagent.getName());
+		String jsonagentName=jsonagent.getName();
+		String serviceStatus = "failure";
+		try {
+	
+		String serviceName = jsonagent.getServicename();
+		String agentName = jsonagent.getName();
+		String agentEmailId = jsonagent.getEmailId();
+		String productivityHours = jsonagent.getProductiveHours();
+		String loginTime = jsonagent.getLoginTime();
+		String logoutTime = jsonagent.getLogoutTime();
+		String idleTimings = jsonagent.getIdleTimings();
 
 		// Validations.
 		if (null == serviceName || "undefined".equalsIgnoreCase(serviceName)
@@ -95,20 +97,20 @@ public class TrackerService {
 		if (null == idleTimings || "undefined".equalsIgnoreCase(idleTimings)) {
 			throw new ValidationException("Validation Exception ::IdleTimingsl is null");
 		}
-		try {
+	
 			// if("error".equalsIgnoreCase(agentName)) {
 			// throw new NullPointerException();
 			// }
 			ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-			String serviceStatus = "failure";
+	
 			if (serviceName.trim().equalsIgnoreCase("storeagentdata")) {
 				AgentDAO dao = (AgentDAO) context.getBean("agentDAO");
 				Agent agent = new Agent();
 				agent.setEmailId(agentEmailId);
 				agent.setName(agentName);
-				String seconds=productivityHours;
-				float minutes=(Float.parseFloat(seconds)/60);
-                agent.setProductiveHours(String.valueOf(minutes));
+				String seconds = productivityHours;
+				float minutes = (Float.parseFloat(seconds) / 60);
+				agent.setProductiveHours(String.valueOf(minutes));
 				agent.setLoginTime(loginTime);
 				agent.setLogoutTime(logoutTime);
 				int count = dao.totalAgentCountInChromeMater(agentEmailId, loginTime);
@@ -171,8 +173,198 @@ public class TrackerService {
 				Agent agent = new Agent();
 				agent.setEmailId(agentEmailId);
 				agent.setName(agentName);
-				String seconds=productivityHours;
-				float minutes=(Float.parseFloat(seconds)/60);
+				String seconds = productivityHours;
+				float minutes = (Float.parseFloat(seconds) / 60);
+				agent.setProductiveHours(String.valueOf(minutes));
+				agent.setLoginTime(loginTime);
+				agent.setLogoutTime(logoutTime);
+				int count = dao.totalAgentCountInChromeMater(agentEmailId, loginTime);
+				int status = 0;
+				if (count == 1) {
+					status = dao.dataUpdateInChromeMater(agent);
+				}
+
+				if (status == 1) {
+
+					if (!idleTimings.trim().equalsIgnoreCase("")) {
+						System.out.println("idle==============" + idleTimings);
+						String splitline[] = idleTimings.split("&&");
+						System.out.println("length of idle==============" + splitline.length);
+						for (int i = 0; i < splitline.length; i++) {
+							System.out.println(splitline[i] + "==============" + i);
+							String splitfeilds[] = splitline[i].split("@@");
+							System.out.println(splitfeilds.length + "-length===");
+							String fromTime = splitfeilds[0];
+							String toTime = splitfeilds[1];
+							String websitesVisited = splitfeilds[2];
+							String webistedvisited_updated = "";
+							String websitevistedsplit[] = websitesVisited.split(",");
+							for (int j = 0; j < websitevistedsplit.length; j++) {
+								if (!websitevistedsplit[j].trim().equalsIgnoreCase("")) {
+									if (webistedvisited_updated.trim().equalsIgnoreCase("")) {
+
+										webistedvisited_updated = websitevistedsplit[j].trim();
+									} else {
+
+										webistedvisited_updated = webistedvisited_updated + ","
+												+ websitevistedsplit[j].trim();
+									}
+
+								}
+							}
+
+							Agent updateagent = new Agent();
+							updateagent.setEmailId(agentEmailId);
+							updateagent.setName(agentName);
+							updateagent.setIdleFrom(fromTime);
+							updateagent.setIdleTo(toTime);
+							updateagent.setWebsitesVisited(webistedvisited_updated);
+							int updateStatus = dao.dataInsertionInChromeDetails(updateagent);
+							if (updateStatus >= 0) {
+
+							}
+
+						}
+					}
+				}
+				serviceStatus = "success";
+			}
+
+			return Response.status(200).entity(agentName + " Stored " + serviceStatus).build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				throw new CustomException();
+			} catch (CustomException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+		return Response.status(200).entity(jsonagentName + " Stored " + serviceStatus).build();
+	}
+
+	/**
+	 * Stores Agent idle hours from extension. agentName, EmailId,
+	 * (IdleHours)From, To, WebsitesUsed sampleUrl ::
+	 * http://localhost:8082/TimeTracker/rest/agent/
+	 * 
+	 * @return
+	 * @throws ValidationException
+	 * @throws CustomException
+	 */
+	@POST
+	@Path("/{serviceName}/{agentName}/{agentEmailId}/{productivityHours}/{loginTime}/{logoutTime}/{idleTimings}")
+	public Response storeAgentData(@PathParam("serviceName") String serviceName,
+			@PathParam("agentName") String agentName, @PathParam("agentEmailId") String agentEmailId,
+			@PathParam("productivityHours") String productivityHours, @PathParam("loginTime") String loginTime,
+			@PathParam("logoutTime") String logoutTime, @PathParam("idleTimings") String idleTimings)
+			throws ValidationException, CustomException {
+		System.out.println(AppConstants.METHOD + "-storeAgentData()");
+
+		// Validations.
+		if (null == serviceName || "undefined".equalsIgnoreCase(serviceName)
+				|| "".trim().equalsIgnoreCase(serviceName)) {
+			throw new ValidationException("Validation Exception :: Service name is empty");
+		}
+		if (null == agentName || "undefined".equalsIgnoreCase(agentName) || "".trim().equalsIgnoreCase(agentName)) {
+			throw new ValidationException("Validation Exception :: Agent name is empty");
+		}
+		if (null == agentEmailId || "undefined".equalsIgnoreCase(agentEmailId)
+				|| "".trim().equalsIgnoreCase(agentEmailId)) {
+			throw new ValidationException("Validation Exception :: Agent Email is empty");
+		}
+		if (null == productivityHours || "undefined".equalsIgnoreCase(productivityHours)
+				|| "".trim().equalsIgnoreCase(productivityHours)) {
+			throw new ValidationException("Validation Exception :: Productivity Hours is empty");
+		}
+		if (null == loginTime || "undefined".equalsIgnoreCase(loginTime) || "".trim().equalsIgnoreCase(loginTime)) {
+			throw new ValidationException("Validation Exception :: Login Time is empty");
+		}
+		if (null == logoutTime || "undefined".equalsIgnoreCase(logoutTime) || "".trim().equalsIgnoreCase(logoutTime)) {
+			throw new ValidationException("Validation Exception :: Logout Time is empty");
+		}
+		if (null == idleTimings || "undefined".equalsIgnoreCase(idleTimings)) {
+			throw new ValidationException("Validation Exception ::IdleTimingsl is null");
+		}
+		try {
+			// if("error".equalsIgnoreCase(agentName)) {
+			// throw new NullPointerException();
+			// }
+			ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+			String serviceStatus = "failure";
+			if (serviceName.trim().equalsIgnoreCase("storeagentdata")) {
+				AgentDAO dao = (AgentDAO) context.getBean("agentDAO");
+				Agent agent = new Agent();
+				agent.setEmailId(agentEmailId);
+				agent.setName(agentName);
+				String seconds = productivityHours;
+				float minutes = (Float.parseFloat(seconds) / 60);
+				agent.setProductiveHours(String.valueOf(minutes));
+				agent.setLoginTime(loginTime);
+				agent.setLogoutTime(logoutTime);
+				int count = dao.totalAgentCountInChromeMater(agentEmailId, loginTime);
+				int status;
+				if (count == 0) {
+					status = dao.dataInsertionInChromeMater(agent);
+				} else {
+
+					status = dao.dataUpdateInChromeMater(agent);
+				}
+
+				if (status >= 0) {
+
+					if (!idleTimings.trim().equalsIgnoreCase("")) {
+						System.out.println("idle==============" + idleTimings);
+						String splitline[] = idleTimings.split("&&");
+						System.out.println("length of idle==============" + splitline.length);
+						for (int i = 0; i < splitline.length; i++) {
+							System.out.println(splitline[i] + "==============" + i);
+							String splitfeilds[] = splitline[i].split("@@");
+							System.out.println(splitfeilds.length + "-length===");
+							String fromTime = splitfeilds[0];
+							String toTime = splitfeilds[1];
+							String websitesVisited = splitfeilds[2];
+							String webistedvisited_updated = "";
+							String websitevistedsplit[] = websitesVisited.split(",");
+							for (int j = 0; j < websitevistedsplit.length; j++) {
+								if (!websitevistedsplit[j].trim().equalsIgnoreCase("")) {
+									if (webistedvisited_updated.trim().equalsIgnoreCase("")) {
+
+										webistedvisited_updated = websitevistedsplit[j].trim();
+									} else {
+
+										webistedvisited_updated = webistedvisited_updated + ","
+												+ websitevistedsplit[j].trim();
+									}
+
+								}
+							}
+
+							Agent updateagent = new Agent();
+							updateagent.setEmailId(agentEmailId);
+							updateagent.setName(agentName);
+							updateagent.setIdleFrom(fromTime);
+							updateagent.setIdleTo(toTime);
+							updateagent.setWebsitesVisited(webistedvisited_updated);
+							int updateStatus = dao.dataInsertionInChromeDetails(updateagent);
+							if (updateStatus >= 0) {
+
+							}
+
+						}
+					}
+				}
+				serviceStatus = "success";
+			}
+
+			if (serviceName.trim().equalsIgnoreCase("storeagentidledata")) {
+				AgentDAO dao = (AgentDAO) context.getBean("agentDAO");
+				Agent agent = new Agent();
+				agent.setEmailId(agentEmailId);
+				agent.setName(agentName);
+				String seconds = productivityHours;
+				float minutes = (Float.parseFloat(seconds) / 60);
 				agent.setProductiveHours(String.valueOf(minutes));
 				agent.setLoginTime(loginTime);
 				agent.setLogoutTime(logoutTime);
--- src/main/java/com/automation/vo/Agent.java
+++ src/main/java/com/automation/vo/Agent.java
@@ -1,7 +1,7 @@
 package com.automation.vo;
 
 public class Agent {
-	
+	private String servicename;
 	private String name;
 	private String emailId;
 	private String idleFrom;
@@ -15,7 +15,23 @@ public class Agent {
 	private String idleHours;
 	private String ErrorDesc;
 	private String recordedTime;
-	
+	private String idleTimings;
+	public String getIdleTimings() {
+		return idleTimings;
+	}
+
+	public void setIdleTimings(String idleTimings) {
+		this.idleTimings = idleTimings;
+	}
+
+	public String getServicename() {
+		return servicename;
+	}
+
+	public void setServicename(String servicename) {
+		this.servicename = servicename;
+	}
+
 	public String getRecordedTime() {
 		return recordedTime;
 	}
@@ -31,8 +47,9 @@ public class Agent {
 	public void setErrorDesc(String errorDesc) {
 		ErrorDesc = errorDesc;
 	}
+
 	private String DATE;
-	
+
 	public String getDATE() {
 		return DATE;
 	}
@@ -45,70 +62,91 @@ public class Agent {
 	public String toString() {
 		return "Agent [name=" + name + ", emailId=" + emailId + "]";
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmailId() {
 		return emailId;
 	}
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
+
 	public String getIdleFrom() {
 		return idleFrom;
 	}
+
 	public void setIdleFrom(String idleFrom) {
 		this.idleFrom = idleFrom;
 	}
+
 	public String getIdleTo() {
 		return idleTo;
 	}
+
 	public void setIdleTo(String idleTo) {
 		this.idleTo = idleTo;
 	}
+
 	public String getWebsitesVisited() {
 		return websitesVisited;
 	}
+
 	public void setWebsitesVisited(String websitesVisited) {
 		this.websitesVisited = websitesVisited;
 	}
+
 	public int getProjectId() {
 		return projectId;
 	}
+
 	public void setProjectId(int projectId) {
 		this.projectId = projectId;
 	}
+
 	public String getShiftTimings() {
 		return shiftTimings;
 	}
+
 	public void setShiftTimings(String shiftTimings) {
 		this.shiftTimings = shiftTimings;
 	}
+
 	public String getLoginTime() {
 		return loginTime;
 	}
+
 	public void setLoginTime(String loginTime) {
 		this.loginTime = loginTime;
 	}
+
 	public String getLogoutTime() {
 		return logoutTime;
 	}
+
 	public void setLogoutTime(String logoutTime) {
 		this.logoutTime = logoutTime;
 	}
+
 	public String getProductiveHours() {
 		return productiveHours;
 	}
+
 	public void setProductiveHours(String productiveHours) {
 		this.productiveHours = productiveHours;
 	}
+
 	public String getIdleHours() {
 		return idleHours;
 	}
+
 	public void setIdleHours(String idleHours) {
 		this.idleHours = idleHours;
 	}
